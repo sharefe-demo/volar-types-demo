e61ba944b7393109addfd23620b4f94a5b6e5b46
sharefe <49937673+sharefe-demo@users.noreply.github.com>
2023-07-02 02:40:41 +0000

    Update 16 files

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
 
-defineProps<{ msg: string }>();
+import type { ObjectEmitsOptions } from 'vue'
 
-const count = ref(0);
+interface EmitsOptions extends /* @vue-ignore */ ObjectEmitsOptions {
+  (event: 'click', data: string): void
+}
+
+const emit = defineEmits<EmitsOptions>()
+
+const handleClick = () => {
+  emit('click', 'You have clicked me!')
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <div :style="{ textAlign: 'center' }">
+    <h3>Hello World</h3>
+    <div style="cursor: pointer; color: red;" @click="handleClick">click me</div>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
